d2d3c1ba208e0243d01bbc0488a529f6cc03fba2
KebabBusiness09 <134933909+KebabBusiness09@users.noreply.github.com>
2023-06-23 16:49:57 +0200

    Update update_keys.py

--- update_keys.py
+++ update_keys.py
@@ -2,15 +2,21 @@ import sys
 import os
 import requests
 
-def update_github_raw(keys):
-    GITHUB_RAW_URL = "https://raw.githubusercontent.com/KebabBusiness09/LICENZE-DA-GENERARE/main/license_keys.txt"
-    GITHUB_ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/KebabBusiness09/LICENZE-DA-GENERARE/main/license_keys.txt"
 
+# Retrieve the GitHub access token from environment variables
+GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
+
+def update_github_raw(keys):
     keys_str = "\n".join(keys)
     headers = {"Authorization": f"token {GITHUB_ACCESS_TOKEN}"}
     response = requests.put(GITHUB_RAW_URL, data=keys_str, headers=headers)
     response.raise_for_status()
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("No keys provided. Please provide comma-separated keys as command-line arguments.")
+        sys.exit(1)
+        
     keys = sys.argv[1].split(",")
     update_github_raw(keys)
